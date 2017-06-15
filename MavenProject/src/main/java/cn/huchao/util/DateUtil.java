2d0aa683e113f504ec0d9d19179e3ef26d6ff515
yilouaitingyu <1970205750@qq.com>
2017-06-15 21:32:39 +0800

    新增工具类

---
+++ MavenProject/src/main/java/cn/huchao/util/DateUtil.java
@@ -0,0 +1,71 @@
+package cn.huchao.util;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * 
+ * @author huchao
+ *	@2017年6月12日
+ *	@description
+ *时间的工具类，还欠缺个日志处理，与异常处理
+ */
+public final class DateUtil {
+	/** Private Constructor **/
+	private DateUtil() {
+	}
+
+	/** 日期格式 **/
+	public interface DATE_PATTERN {
+		String HHMMSS = "HHmmss";
+		String HH_MM_SS = "HH:mm:ss";
+		String YYYYMMDD = "yyyyMMdd";
+		String YYYY_MM_DD = "yyyy-MM-dd";
+		String YYYYMMDDHHMMSS = "yyyyMMddHHmmss";
+		String YYYYMMDDHHMMSSSSS = "yyyyMMddHHmmssSSS";
+		String YYYY_MM_DD_HH_MM_SS = "yyyy-MM-dd HH:mm:ss";
+	}
+	/**
+	 * 将Date类型转换成String类型
+	 * 
+	 * @param date
+	 *            Date对象
+	 * @return 形如:"yyyy-MM-dd HH:mm:ss"
+	 */
+	public static String date2String(Date date) {
+		return date2String(date, DATE_PATTERN.YYYY_MM_DD_HH_MM_SS);
+	}
+
+	/**
+	 * 将Date按格式转化成String
+	 * 
+	 * @param date
+	 *            Date对象
+	 * @param pattern
+	 *            日期类型
+	 * @return String
+	 */
+	public static String date2String(Date date, String pattern) {
+		if (date == null || pattern == null){
+			return null;
+		}
+		return new SimpleDateFormat(pattern).format(date);
+	}
+
+	/**
+	 * 将String类型转换成Date类型
+	 * 
+	 * @param date
+	 *            Date对象
+	 * @return
+	 */
+	public static Date string2Date(String date) {
+		SimpleDateFormat format = new SimpleDateFormat(DATE_PATTERN.YYYY_MM_DD_HH_MM_SS);
+		try {
+			return format.parse(date);
+		} catch (ParseException e) {
+			return null;
+		}
+	}
+}
